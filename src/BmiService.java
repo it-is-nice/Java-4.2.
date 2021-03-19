28abfe95114a318f6aad0c462e0571190954975a
Max Vertlib <maximvert@gmail.com>
2021-03-19 09:57:10 +0300

    Modified src

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public double calculate (int weight, double height) {
         height = height / 100;
+
         double bmi = weight / height /height;
 
     return bmi;
